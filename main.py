bea3e2d8d6e6b7899e18bd6c7cbcb4bcc1044bd2
Alex Breeze <91281967+YaBoiBreezy@users.noreply.github.com>
2023-07-20 22:05:36 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+#Predictive audio generator
+#Alexander Breeze
+#Using wav files, uncompressed, PCM format
+#PCM (Pulse Code Modulation) is series of ints, each int is amplitude of sound wave at timestamp
