3dfa73aeeafcbf5b9c850f3a4a9bbe7729f57d8e
Alex Breeze <91281967+YaBoiBreezy@users.noreply.github.com>
2023-07-23 18:07:45 -0400

    got audio interfacing done

--- main.py
+++ main.py
@@ -2,3 +2,40 @@
 #Alexander Breeze
 #Using wav files, uncompressed, PCM format
 #PCM (Pulse Code Modulation) is series of ints, each int is amplitude of sound wave at timestamp
+
+import sounddevice as sd
+import soundfile as sf
+from scipy.io.wavfile import write
+import numpy as np
+import tensorflow
+
+#globals
+fs = 44100  # Sample rate
+ch = 1 #number of channels
+
+def recordWav(time, fileName):
+ myrecording = sd.rec(int(time * fs), samplerate=fs, channels=ch)
+ sd.wait()  # wait until recording is finished
+ write(fileName, fs, myrecording)  # save as WAV file
+
+def playWavFile(fileName):
+ data, b = sf.read(fileName, dtype='float32')
+ sd.play(data, fs)
+ status = sd.wait()  # wait until file is done playing
+
+def readWavFile(fileName):
+ data, b = sf.read(fileName, dtype='float32')
+ return data
+
+def playWav(data):
+ sd.play(data, fs)
+ status = sd.wait()  # wait until file is done playing
+
+
+
+
+
+
+
+
+
